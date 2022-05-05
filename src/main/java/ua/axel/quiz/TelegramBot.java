c45b38a9b5432f998adccfb70cc06c35cbd13be5
AxelNordov <iq@ua.fm>
2022-05-05 09:08:26 +0300

    add Lombok
    update TelegramBot getters

--- src/main/java/ua/axel/quiz/TelegramBot.java
+++ src/main/java/ua/axel/quiz/TelegramBot.java
@@ -1,5 +1,6 @@
 package ua.axel.quiz;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -8,6 +9,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@Getter
 public class TelegramBot extends TelegramLongPollingBot {
 
 	@Value("${telegram.bot-username}")
@@ -29,15 +31,5 @@ public class TelegramBot extends TelegramLongPollingBot {
 				e.printStackTrace();
 			}
 		}
-	}
-
-	@Override
-	public String getBotUsername() {
-		return botUsername;
-	}
-
-	@Override
-	public String getBotToken() {
-		return botToken;
 	}
 }
