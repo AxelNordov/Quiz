620ddb7f194163c554ee013e028d7e4a356ad069
AxelNordov <iq@ua.fm>
2022-05-05 09:45:05 +0300

    refactor TelegramBot
    extract methods

--- src/main/java/ua/axel/quiz/TelegramBot.java
+++ src/main/java/ua/axel/quiz/TelegramBot.java
@@ -4,9 +4,13 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.Serializable;
+import java.util.Optional;
 
 @Component
 @Getter
@@ -20,16 +24,24 @@ public class TelegramBot extends TelegramLongPollingBot {
 
 	@Override
 	public void onUpdateReceived(Update update) {
+		getMethod(update).ifPresent(this::executeMethod);
+	}
+
+	private Optional<BotApiMethod<? extends Serializable>> getMethod(Update update) {
 		if (update.hasMessage() && update.getMessage().hasText()) {
 			SendMessage message = new SendMessage();
 			message.setChatId(update.getMessage().getChatId().toString());
 			message.setText(update.getMessage().getText());
+			return Optional.of(message);
+		}
+		return Optional.empty();
+	}
 
-			try {
-				execute(message);
-			} catch (TelegramApiException e) {
-				e.printStackTrace();
-			}
+	private void executeMethod(BotApiMethod<? extends Serializable> method) {
+		try {
+			execute(method);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
 		}
 	}
 }
