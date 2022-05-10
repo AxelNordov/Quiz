aa5572128ac72107a29801d6c20ed051f2b0da9f
AxelNordov <iq@ua.fm>
2022-05-10 17:41:20 +0300

    distribute the number of buttons relatively evenly

--- src/main/java/ua/axel/quiz/service/KeyboardService.java
+++ src/main/java/ua/axel/quiz/service/KeyboardService.java
@@ -5,21 +5,36 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class KeyboardService {
+
+	public static final int MAX_BUTTONS_NUMBER_IN_ROW = 4;
+
 	public ReplyKeyboardMarkup getMainMenuKeyboard(String... buttonText) {
 		final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 		replyKeyboardMarkup.setSelective(true);
 		replyKeyboardMarkup.setResizeKeyboard(true);
 		replyKeyboardMarkup.setOneTimeKeyboard(false);
-		replyKeyboardMarkup.setKeyboard(List.of(
-				new KeyboardRow(Arrays.stream(buttonText)
-						.map(KeyboardButton::new)
-						.collect(Collectors.toList()))));
+		List<KeyboardRow> keyboard = new ArrayList<>();
+		List<KeyboardButton> buttons = Arrays.stream(buttonText)
+				.map(KeyboardButton::new)
+				.collect(Collectors.toList());
+		// Distribute the number of buttons relatively evenly.
+		// Yes, I know it needs to be improved. But it works.
+		int maxButtonsNumberInRow = MAX_BUTTONS_NUMBER_IN_ROW;
+		for (int i = 0; i < buttons.size(); i += maxButtonsNumberInRow) {
+			if (buttons.size() - i > maxButtonsNumberInRow && buttons.size() - i < maxButtonsNumberInRow * 1.5) {
+				maxButtonsNumberInRow--;
+			}
+			keyboard.add(new KeyboardRow(
+					buttons.subList(i, Math.min(i + maxButtonsNumberInRow, buttonText.length))));
+		}
+		replyKeyboardMarkup.setKeyboard(keyboard);
 		return replyKeyboardMarkup;
 	}
 }
