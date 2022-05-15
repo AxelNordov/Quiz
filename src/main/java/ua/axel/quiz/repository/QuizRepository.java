96155d306e65865ca2c9bfb8aab85a3b3348e145
AxelNordov <iq@ua.fm>
2022-05-15 16:46:27 +0300

    add easy play logic

--- src/main/java/ua/axel/quiz/repository/QuizRepository.java
+++ src/main/java/ua/axel/quiz/repository/QuizRepository.java
@@ -1,9 +1,18 @@
 package ua.axel.quiz.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ua.axel.quiz.entity.Quiz;
 
 @Repository
 public interface QuizRepository extends CrudRepository<Quiz, Long> {
+	@Query(value = "" +
+			"SELECT * " +
+			"FROM quiz " +
+			"WHERE right_answer > 0 " +
+			"ORDER BY random() " +
+			"LIMIT 1;", nativeQuery = true)
+	Quiz findRandomQuizWithRightAnswer();
+
 }
--- src/main/java/ua/axel/quiz/service/QuizService.java
+++ src/main/java/ua/axel/quiz/service/QuizService.java
@@ -2,11 +2,21 @@ package ua.axel.quiz.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ua.axel.quiz.entity.Answer;
 import ua.axel.quiz.entity.Quiz;
 import ua.axel.quiz.repository.QuizRepository;
 
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class QuizService {
+	private static final String QUIZ_TYPE = "quiz";
 	@Autowired
 	private QuizRepository quizRepository;
 
@@ -16,5 +26,31 @@ public class QuizService {
 
 	public void save(Quiz quiz) {
 		quizRepository.save(quiz);
+	}
+
+	public Optional<BotApiMethod<Message>> getSendPool(Update update) {
+		var message = update.getMessage();
+		var chatId = message.getChatId().toString();
+		var quiz = quizRepository.findRandomQuizWithRightAnswer();
+		var sendPoll = getSendPollFromQuiz(chatId, quiz);
+		return Optional.of(sendPoll);
+	}
+
+	private SendPoll getSendPollFromQuiz(String chatId, Quiz quiz) {
+		var options = quiz.getAnswers().stream()
+				.sorted(Comparator.comparingInt(Answer::getOrderNumber))
+				.map(Answer::getBody)
+				.collect(Collectors.toList());
+		return SendPoll.builder()
+				.chatId(chatId)
+				.options(options)
+				.question(quiz.getQuestion())
+				.type(QUIZ_TYPE)
+				.correctOptionId(getCorrectOptionId(quiz.getRightAnswer()))
+				.build();
+	}
+
+	private int getCorrectOptionId(Byte rightAnswer) {
+		return rightAnswer - 1;
 	}
 }
